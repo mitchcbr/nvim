72bfb78cf7a4e1ac5bf7317f3416a835dfa038cf
Mitch <78887076+mitchcbr@users.noreply.github.com>
2025-02-17 13:26:08 -0600

    switch toggleterm to float

--- init.lua
+++ init.lua
@@ -4,17 +4,23 @@ require("config.lazy")
 require("toggleterm").setup({
   size = 80,
   open_mapping = [[<c-\>]], -- or { [[<c-\>]], [[<c-¥>]] } if you also use a Japanese keyboard.
-  autochdir = true, -- when neovim changes it current directory the terminal will change it's own when next it's opened
+  autochdir = true,         -- when neovim changes it current directory the terminal will change it's own when next it's opened
   start_in_insert = true,
-  insert_mappings = true, -- whether or not the open mapping applies in insert mode
+  insert_mappings = true,   -- whether or not the open mapping applies in insert mode
   terminal_mappings = true, -- whether or not the open mapping applies in the opened terminals
   persist_size = true,
-  persist_mode = true, -- if set to true (default) the previous terminal mode will be remembered
-  direction = "vertical",
+  persist_mode = true,      -- if set to true (default) the previous terminal mode will be remembered
+  -- direction = 'vertical',
+  direction = 'float',
   close_on_exit = true, -- close the terminal window when the process exits
   shell = vim.o.shell,
-  auto_scroll = true, -- automatically scroll to the bottom on terminal output
+  auto_scroll = true,   -- automatically scroll to the bottom on terminal output
   winbar = {
     enabled = false,
+  },
+  float_opts = {
+    border = 'curved',
+    winblend = 10,
+    title_pos = 'center'
   },
 })
